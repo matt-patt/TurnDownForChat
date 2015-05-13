ef5b70f6f8241c25c9f20fba86be7f7574231738
matt patt <matt-patt@users.noreply.github.com>
2015-05-13 13:01:24 -0400

    Update ConnectListener.java
    updated comments

--- server/ConnectListener.java
+++ server/ConnectListener.java
@@ -3,6 +3,7 @@ package server;
 import java.io.IOException;
 import java.net.ServerSocket;
 
+//listens for new Connection attempts and passes them to the Server object
 public class ConnectListener implements Runnable{
 	private ServerSocket server;
 	private Server srv;
@@ -14,13 +15,13 @@ public class ConnectListener implements Runnable{
 	}
 	
 	public void run(){ // listens for new connections
-		try{
+		try{ // creates the ServerSocket to listen for connections
 			server = new ServerSocket(port);
 		} catch(IOException e){
 			e.printStackTrace();
 		}
 		while(true) {
-			try {
+			try { // if a client attempts to connect tell the server
 				srv.addConnection(server.accept());
 			} catch (IOException e) {
 				e.printStackTrace();
